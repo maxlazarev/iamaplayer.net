cc8f060fe3e2bcec27d19756b01012e6aa099cbd
diedsmiling <diedsmiling@gmail.com>
2015-08-29 02:02:02 +0300

    Invalid credintals test fix

--- tests/backend/routes/auth.test.js
+++ tests/backend/routes/auth.test.js
@@ -8,7 +8,7 @@ describe('Auth', function() {
 
     });
 
-    it('should handle 401 request on login failure', function() {
+    it('should handle 401 request with invalid credintals', function() {
         res = {
             status: sinon.spy(),
             json:    sinon.spy()
@@ -23,7 +23,7 @@ describe('Auth', function() {
         expect(res.status).to.be.calledWith(401);
         expect(res.json).to.be.calledWith({
             status:     401,
-            meassage:   'Invalid credentials'
+            meassage:   'Invalid credintals'
         });
     });
 });
