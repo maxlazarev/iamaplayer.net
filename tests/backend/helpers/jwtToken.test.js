15f7fe7946705780b5225a6c719b9dc134c29ff1
diedsmiling <diedsmiling@gmail.com>
2015-09-01 23:35:12 +0300

    jwtToken helper. setExpirationDate method

--- tests/backend/helpers/jwtToken.test.js
+++ tests/backend/helpers/jwtToken.test.js
@@ -7,10 +7,14 @@ describe('Jwt token helper',function() {
         it('should create date object', function() {
             var Date = sinon.stub(global, 'Date');
             Date.now = sinon.spy();
+            Date.prototype.getDate = sinon.spy();
+            Date.prototype.setDate = sinon.spy();
 
             jwtHelper.setExpirationDate(global.TOKEN_EXPIRE_DAYS);
 
             expect(Date).to.be.called;
+            expect(Date.prototype.getDate).to.be.called;
+            expect(Date.prototype.setDate).to.be.calledWith(Date.prototype.getDate() + global.TOKEN_EXPIRE_DAYS);
         });
     });
 
