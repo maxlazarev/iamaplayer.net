81c199cabbbab35372cf45e07c9ca9b24a42b240
diedsmiling <diedsmiling@gmail.com>
2015-09-02 21:46:24 +0300

    jwtToken helper. generateToken return test

--- tests/backend/helpers/jwtToken.test.js
+++ tests/backend/helpers/jwtToken.test.js
@@ -1,12 +1,17 @@
 require('../../../constants')();
 var jwtHelper   = require('../../../helpers/jwtToken');
 var proxyquire  = require('proxyquire');
+var dateTemp = Date;
 
 describe('Jwt token helper',function() {
 
     describe('setExpirationDate method', function() {
+        afterEach(function() {
+            Date = dateTemp;
+        });
+
         it('should create date object', function() {
-            var Date = sinon.stub(global, 'Date');
+            Date = sinon.stub(global, 'Date');
             Date.now = sinon.spy();
             Date.prototype.getDate = sinon.spy();
             Date.prototype.setDate = sinon.spy();
@@ -20,16 +25,31 @@ describe('Jwt token helper',function() {
     });
 
     describe('genToken method', function() {
+        it('should return object', function() {
+            var userStub = {
+                email: 'email@email.com'
+            };
+            jwtHelper.genToken = sinon.spy(jwtHelper, 'genToken');
+            jwtHelper.genToken(userStub);
+
+            expect(jwtHelper.genToken).returned({
+                token:      sinon.match.string,
+                expires:    sinon.match.number,
+                user:       userStub
+            });
+        });
 
         it('should call setExpirationDate method', function() {
+            var userStub = sinon.stub();
             jwtHelper.setExpirationDate = sinon.spy();
 
-            jwtHelper.genToken();
+            jwtHelper.genToken(userStub);
 
             expect(jwtHelper.setExpirationDate).to.be.calledWith(global.TOKEN_EXPIRE_DAYS);
         });
 
         it('should generate token', function() {
+            var userStub = sinon.stub();
             var jwtStub = {
                 encode: sinon.spy()
             };
@@ -37,12 +57,13 @@ describe('Jwt token helper',function() {
             jwtHelper = proxyquire('../../../helpers/jwtToken', {
                 'jwt-simple': jwtStub
             });
+
             jwtHelper.setExpirationDate = sinon.stub().returns(new Date());
 
-            jwtHelper.genToken();
+            jwtHelper.genToken(userStub);
 
             expect(jwtStub.encode).to.be.calledWith({
-                exp: sinon.match.object
+                exp: sinon.match.date
             }, global.TOKEN_SECRET);
         });
 
