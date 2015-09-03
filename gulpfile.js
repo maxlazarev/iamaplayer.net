7128f0df754cbeabc5e43435c8012be8799a0ad0
diedsmiling <diedsmiling@gmail.com>
2015-09-04 00:36:28 +0300

    Gulp css tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp            = require('gulp');
 var plugins         = require('gulp-load-plugins')();
+var clip            = require('gulp-clip-empty-files');
 var appsPaths       = require('./server/paths');
 var apps            = Object.keys(appsPaths);
 
@@ -49,6 +50,39 @@ function buildSprite(appName, paths) {
         spriteData.css.pipe(gulp.dest(paths.cssSrc));
     });
 }
+/**
+ * Compiles scss files
+ *
+ * @param {str} appName Application name
+ * @param {obj} paths Application paths
+ */
+function compileScss(appName, paths) {
+    gulp.task('compile_scss_' + appName, ['build_sprite_' + appName], function() {
+        return gulp.src(paths.scssSrc)
+            .pipe(plugins.debug({title: 'Scss src'}))
+            .pipe(plugins.if(/scss$/, plugins.sass()))
+            .pipe(clip())
+            .pipe(gulp.dest(paths.cssSrc))
+            .pipe(plugins.debug({title: 'Scss dist'}));
+    });
+}
+
+
+/**
+ * Concatinates and minifies css files
+ *
+ * @param {str} appName Application name
+ * @param {obj} paths Application paths
+ */
+function buildCompressedCss(appName, paths) {
+    gulp.task('compress_css_' + appName, ['compile_scss_' + appName], function() {
+        return gulp.src(paths.cssSrc + '/**/*.css')
+            .pipe(plugins.debug({title: 'Css src'}))
+            .pipe(plugins.concat('app.css'))
+            .pipe(plugins.minifyCss())
+            .pipe(gulp.dest(paths.cssDist));
+    });
+}
 
 /**
  * Build tasks for both apps "admin" and "front"
@@ -57,11 +91,21 @@ apps.forEach(function(appName) {
     var paths = appsPaths[appName];
     buildDevIndexFileTask(appName, paths);
     buildSprite(appName, paths);
+    compileScss(appName, paths);
+    buildCompressedCss(appName, paths);
 });
 
-gulp.task('watch', ['build_index_admin', 'build_index_front'], function() {
+gulp.task('watch', [
+    'build_index_admin',
+    'build_index_front',
+    'compress_css_admin',
+    'compress_css_front'
+    ], function() {
     apps.forEach(function(appName) {
         gulp.watch(appsPaths[appName].indexSrc, ['build_index_' + appName]);
+        gulp.watch(appsPaths[appName].spriteSrc,['build_sprite_' + appName]);
+        gulp.watch(appsPaths[appName].scssSrc,  ['compile_scss_' + appName]);
+        gulp.watch(appsPaths[appName].cssSrc + '/**/*.css',   ['compress_css_' + appName]);
     });
 });
 
--- server/paths.js
+++ server/paths.js
@@ -8,7 +8,8 @@ module.exports = {
 
         jsSrc:      [rootDir + '/app/js/app.js', rootDir + 'app/js/**/*.js'],
         tplsSrc:    rootDir + '/app/js/**/*.tpl.html',
-        cssSrc:     rootDir + '/app/scss/',
+        scssSrc:    rootDir + '/app/scss/',
+        cssSrc:     rootDir + '/app/css',
         indexSrc:   rootDir + '/app/index.html',
         spriteSrc:  rootDir + '/app/sprite_src/*',
 
@@ -17,7 +18,7 @@ module.exports = {
          */
 
         jsDist:     'public/js/app.js',
-        cssDist:    'public/css/app.css',
+        cssDist:    'public/css/',
         indexDist:  'public/app.html',
         distDir:    'public',
         imgsDist:   'public/img'
@@ -30,7 +31,8 @@ module.exports = {
 
         jsSrc:      [rootDir + '/admin_app/js/app.js', rootDir + 'admin_app/js/**/*.js'],
         tplsSrc:    rootDir + '/admin_app/js/**/*.tpl.html',
-        cssSrc:     rootDir + '/admin_app/scss/',
+        cssSrc:     rootDir + '/admin_app/css/',
+        scssSrc:    rootDir + '/admin_app/scss/*.scss',
         indexSrc:   rootDir + '/admin_app/index.html',
         spriteSrc:  rootDir + '/admin_app/sprite_src/*',
 
@@ -39,7 +41,7 @@ module.exports = {
          */
 
         jsDist:     'public/admin/js/app.js',
-        cssDist:    'public/admin/js/app.css',
+        cssDist:    'public/admin/css',
         indexDist:  'public/admin/app.html',
         distDir:    'public/admin',
         imgsDist:   'public/admin/img'
