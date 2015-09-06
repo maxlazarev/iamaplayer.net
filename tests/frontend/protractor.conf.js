21262c932196f620fcf1fe2098a49b4747dfa286
diedsmiling <diedsmiling@gmail.com>
2015-09-06 18:28:05 +0300

    Protractor init

---
+++ tests/frontend/protractor.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+    allScriptsTimeout: 11000,
+
+    specs: [
+        'e2e/**/*.spec.js'
+    ],
+
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    chromeOnly: true,
+
+    baseUrl: 'http://localhost:3300/',
+
+    framework: 'mocha',
+
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
