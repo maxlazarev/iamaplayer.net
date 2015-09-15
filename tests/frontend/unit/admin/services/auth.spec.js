e1f5d3f85449a23a90ab0e2a0ea0aa494b29ad27
diedsmiling <diedsmiling@gmail.com>
2015-09-16 00:16:51 +0300

    Auth service

--- tests/frontend/unit/admin/services/auth.spec.js
+++ tests/frontend/unit/admin/services/auth.spec.js
@@ -2,28 +2,44 @@
 var mockHttp;
 var mockLocalStorage;
 var mockLocation;
-var mockAuth;
+var AuthObj;
+var $http = {
+    post: function() {
+        console.log('post request ...');
+    }
+};
 
 describe('Auth service', function() {
     beforeEach(function() {
-        module(function($provide) {
+        module(function($provide, $injector) {
             $provide.service('$http', function() {
-                //mock a mehtod
+                spyOn($http, 'post').and.callFake(function() {
+                    return {
+                        token:  'token_string',
+                        user:   {
+                            data: 'some_data'
+                        }
+                    };
+                });
             });
             $provide.service('modalSvc', function() {
                 //mock a method
             });
+
         });
         module('adminApp');
 
-        inject(function($http, $location, Auth) {
+        inject(function($http, $location, Auth, $injector) {
+            $http = $injector.get('$http');
             mockHttp = $http;
             mockLocation = $location;
-            mockAuth = Auth;
+            AuthObj = Auth;
+
         });
     });
 
     it('it should decode token', function() {
+        AuthObj.login({data: 'data'});
         expect();
     });
 });
